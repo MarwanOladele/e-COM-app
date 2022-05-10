d7481500ffcbe4e368ac8928e86dc24ec5300636
Oladele Marwan <oladelemarwan@gmail.com>
2022-05-10 20:26:53 +0100

    new commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 6rem;
+`;
+
+const Navbar = () => {
+  return <Container>Navbar</Container>;
+};
+
+export default Navbar;
