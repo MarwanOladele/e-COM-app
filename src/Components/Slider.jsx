de82c9a7db907b04a3f1bb2cf837e854c33dd8cd
Oladele Marwan <oladelemarwan@gmail.com>
2022-05-11 19:25:56 +0100

    new commit

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,35 +1,50 @@
-import React from 'react'
+import React from "react";
 
-import styled from 'styled-components'
-import {AiOutlineArrowRight, AiOutlineArrowLeft} from 'react-icons/ai'
-
+import styled from "styled-components";
+import { AiOutlineArrowRight, AiOutlineArrowLeft } from "react-icons/ai";
 
 const Container = styled.div`
-width: 100%;
-height: 100vh;
-display: flex;
-background-color: gray
-`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  position: relative;
+`;
 
 const Arrow = styled.div`
-width: 50px;
-height: 50px
-background-color: black;
-border-radius:  50%;
+  font-size: 20px;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+  cursor: pointer;
+  opacity: 0.5;
+`;
 
-`
+const Wrapper = styled.div`
+  height: 100%;
+`;
+
+
 
 const Slider = () => {
   return (
-   <Container>      
-       <Arrow>
-           <AiOutlineArrowLeft/>
-       </Arrow>
-        <Arrow>
-          <AiOutlineArrowRight/>
-       </Arrow>
-   </Container>
-  )
-}
+    <Container>
+      <Arrow direction="left">
+        <AiOutlineArrowLeft />
+      </Arrow>
+      <Wrapper>
 
-export default Slider
+      </Wrapper>
+      <Arrow direction="right">
+        <AiOutlineArrowRight />
+      </Arrow>
+    </Container>
+  );
+};
+
+export default Slider;
