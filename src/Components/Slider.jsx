abc983f08add92f7fc94f059a89b3ebb23608538
Oladele Marwan <oladelemarwan@gmail.com>
2022-05-12 13:03:51 +0100

    new commit

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import styled from "styled-components";
 import { AiOutlineArrowRight, AiOutlineArrowLeft } from "react-icons/ai";
+import img1 from "../Images/img1.png";
 
 const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
   position: relative;
+  overflow: hidden;
 `;
 
 const Arrow = styled.div`
@@ -27,20 +29,95 @@ const Arrow = styled.div`
 
 const Wrapper = styled.div`
   height: 100%;
+  display: flex;
 `;
 
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  background-color: #${(props) => props.bg};
+`;
+const ImgContainer = styled.div`
+  flex: 1;
+  height: 100%;
+`;
+const Image = styled.img`
+  height: 100%;
+  width: 100%;
+`;
 
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+
+const Title = styled.h1`
+  font-size: 70px;
+`;
+const Desc = styled.p`
+  margin: 30px 0px;
+  font-size: 20px;
+  font-weight: bold;
+  letter-spacing: 3px;
+`;
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
+`;
 
 const Slider = () => {
+  const handleClick = (direction) => {
+      
+  };
+
   return (
     <Container>
-      <Arrow direction="left">
+      <Arrow direction="left" onClick={() => handleClick("left")}>
         <AiOutlineArrowLeft />
       </Arrow>
       <Wrapper>
-
+        <Slide bg="f5fafd">
+          <ImgContainer>
+            <Image src={img1} />
+          </ImgContainer>
+          <InfoContainer>
+            <Title>SUMMER SALE</Title>
+            <Desc>
+              DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS
+            </Desc>
+            <Button>SHOP NOW</Button>
+          </InfoContainer>
+        </Slide>
+        <Slide bg="fbf0f4">
+          <ImgContainer>
+            <Image src={img1} />
+          </ImgContainer>
+          <InfoContainer>
+            <Title>WINTER SALE</Title>
+            <Desc>
+              DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS
+            </Desc>
+            <Button>SHOP NOW</Button>
+          </InfoContainer>
+        </Slide>
+        <Slide bg="orange">
+          <ImgContainer>
+            <Image src={img1} />
+          </ImgContainer>
+          <InfoContainer>
+            <Title>POPULAR SAL</Title>
+            <Desc>
+              DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS
+            </Desc>
+            <Button>SHOP NOW</Button>
+          </InfoContainer>
+        </Slide>
       </Wrapper>
-      <Arrow direction="right">
+      <Arrow direction="right" onClick={() => handleClick("right")}>
         <AiOutlineArrowRight />
       </Arrow>
     </Container>
