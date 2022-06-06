f54b16a90afe8699465ed583eaf0d165294bacc3
Oladele Marwan <oladelemarwan@gmail.com>
2022-06-06 18:39:15 +0100

    new commit

--- ecom-app/pages/index.js
+++ ecom-app/pages/index.js
@@ -1,15 +1,18 @@
 import React from "react";
+import { HeroBanner, Footer, products } from "../components/index";
 
 const Home = () => {
   return (
     <>
-      HeroBanner
-      <div>
+      <HeroBanner />
+      <div className="products-heading">
         <h2>Best selling products</h2>
         <p>Speakers of many variation</p>
       </div>
-      <div>{["Product 1", "Product 2"].map((product) => product)}</div>
-      Footer
+      <div className="products-heading">
+        {["Product 1", "Product 2"].map((product) => product)}
+      </div>
+      <Footer />
     </>
   );
 };
