db084e05dac5c3927068f0d3e7831ddad5869ac4
Oladele Marwan <oladelemarwan@gmail.com>
2022-06-13 22:06:35 +0100

    new commit

--- ecom-app/pages/product/[slug].js
+++ ecom-app/pages/product/[slug].js
@@ -1,20 +1,62 @@
 import React from "react";
 import { urlFor, client } from "../../lib/client";
+import {
+  AiOutlineMinus,
+  AiOutlinePlus,
+  AiFillStar,
+  AiOutlineStar,
+} from "react-icons/ai";
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
 
   return (
-    <div className="product-detail-container">
-      <div>
-        <div className="image-container">
-          <img src={urlFor(image && image[0])} />
+    <div>
+      <div className="product-detail-container">
+        <div>
+          <div className="image-container">
+            <img src={urlFor(image && image[0])} />
+          </div>
+          {/* <div className="small-images-container">
+            {image?.map((item, i) => (
+              <img src={urlFor(item)} className="" onMouseOver="" />
+            ))}
+          </div> */}
+        </div>
+        <div className="product-details-desc">
+          <h1>{name}</h1>
+          <div className="reviews">
+            <div>
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiOutlineStar />
+            </div>
+            <p>(20)</p>
+          </div>
+          <h4>Details: </h4>
+          <p>{details}</p>
+          <p className="price">${price}</p>
+          <div className="quantity">
+            <h3>Quantity: </h3>
+            <p className="quantity-desc">
+              <span className="minus" onClick="">
+                <AiOutlineMinus />
+              </span>
+              <span className="num" onClick="">
+               0
+              </span>
+              <span className="plus" onClick="">
+                <AiOutlinePlus />
+              </span>
+            </p>
+          </div>
         </div>
       </div>
     </div>
   );
 };
-
 
 export const getStaticPaths = async () => {
   const query = `*[_type == "product"] {
@@ -27,30 +69,28 @@ export const getStaticPaths = async () => {
   const products = await client.fetch(query);
 
   const paths = products.map((product) => ({
-    params: { 
-      slug: product.slug.current
-    }
+    params: {
+      slug: product.slug.current,
+    },
   }));
 
   return {
     paths,
-    fallback: 'blocking'
-  }
-}
+    fallback: "blocking",
+  };
+};
 
-export const getStaticProps = async ({ params: { slug }}) => {
+export const getStaticProps = async ({ params: { slug } }) => {
   const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
-  const productsQuery = '*[_type == "product"]'
-  
+  const productsQuery = '*[_type == "product"]';
+
   const product = await client.fetch(query);
   const products = await client.fetch(productsQuery);
 
   console.log(product);
 
   return {
-    props: { products, product }
-  }
-
-  
-}
+    props: { products, product },
+  };
+};
 export default ProductDetails;
