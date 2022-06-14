a61c0193e6aa07b9dce17370eec385c0c05615a7
Oladele Marwan <oladelemarwan@gmail.com>
2022-06-14 19:37:13 +0100

    new commit

--- ecom-app/pages/product/[slug].js
+++ ecom-app/pages/product/[slug].js
@@ -15,7 +15,10 @@ const ProductDetails = ({ product, products }) => {
       <div className="product-detail-container">
         <div>
           <div className="image-container">
-            <img src={urlFor(image && image[0])} />
+            <img
+              src={urlFor(image && image[0])}
+              className="product-detail-image"
+            />
           </div>
           {/* <div className="small-images-container">
             {image?.map((item, i) => (
@@ -23,7 +26,8 @@ const ProductDetails = ({ product, products }) => {
             ))}
           </div> */}
         </div>
-        <div className="product-details-desc">
+
+        <div className="product-detail-desc">
           <h1>{name}</h1>
           <div className="reviews">
             <div>
@@ -45,13 +49,29 @@ const ProductDetails = ({ product, products }) => {
                 <AiOutlineMinus />
               </span>
               <span className="num" onClick="">
-               0
+                0
               </span>
               <span className="plus" onClick="">
                 <AiOutlinePlus />
               </span>
             </p>
           </div>
+          <div className="buttons">
+            <button className="add-to-cart" onClick="" type="button">
+              Add to Cart
+            </button>
+            <button className="buy-now" onClick="" type="button">
+              Buy now
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="maylike-products-wrapper">
+        <h2>You may also like</h2>
+        <div className="marquee">
+            <div className="maylike-products container">
+                
+            </div>
         </div>
       </div>
     </div>
