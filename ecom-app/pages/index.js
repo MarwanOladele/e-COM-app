d615042389c9177b5efcba1c4f0eb8a1de62b9ed
Oladele Marwan <oladelemarwan@gmail.com>
2022-06-20 22:03:47 +0100

    new commit

--- ecom-app/pages/index.js
+++ ecom-app/pages/index.js
@@ -1,24 +1,25 @@
 import React from "react";
-import { HeroBanner, FooterBanner, Products } from "../components/index";
-import { client } from "../lib/client";
 
-const Home = ({ products, bannerData }) => {
-  return (
-    <>
-      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
-      <div className="products-heading">
-        <h2>Best selling products</h2>
-        <p>Speakers of many variation</p>
-      </div>
-      <div className="products-container">
-        {products?.map((product) => (
-          <Products key={product._id} {...product} />
-        ))}
-      </div>
-      <FooterBanner footerBanner={bannerData.length && bannerData[0]} />
-    </>
-  );
-};
+import { client } from "../lib/client";
+import { Product, FooterBanner, HeroBanner } from "../components";
+
+const Home = ({ products, bannerData }) => (
+  <div>
+    <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+    <div className="products-heading">
+      <h2>Best Seller Products</h2>
+      <p>speaker There are many variations passages</p>
+    </div>
+
+    <div className="products-container">
+      {products?.map((product) => (
+        <Product key={product._id} product={product} />
+      ))}
+    </div>
+
+    <FooterBanner footerBanner={bannerData && bannerData[0]} />
+  </div>
+);
 
 export const getServerSideProps = async () => {
   const query = '*[_type == "product"]';
